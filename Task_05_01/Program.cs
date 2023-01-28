cb5b8c6e6cf24552fc94cbe10f61b73ea13e1a7a
MMick <mickmail2000@gmail.com>
2023-01-29 00:56:17 +0300

    Добавили в задачу 5 условие с циклом на правильность ввода

--- Task_05_01/Program.cs
+++ Task_05_01/Program.cs
@@ -3,13 +3,26 @@
 // 2. на выходе показывает все целые числа в промежутке от -N до N.
 // 4-> "-4,-3,-2,-1, 0,1, 2,3,4"
 // 2-> "-2,-1, 0,1,2"
+bool isFinished = false;
 
-Console.WriteLine("Введите любое целое число от нуля и больше");
+Console.WriteLine("Введите любое целое число больше нуля:");
+while(isFinished == false) {
 int N = Convert.ToInt32(Console.ReadLine());
-int number = 0 - N;
+if (N > 0){
+    // do{
+ int number = 0 - N;
+ 
 while (number <= N){
     Console.Write(number);
     number ++;
     if (number <= N){
-    Console.Write(", ");}
+    Console.Write(", ");
+    isFinished = true;}
+}   
+}else {
+    Console.WriteLine("Внимательнее читай задание... и снова вводи число");
+    isFinished = false;
 }
+}
+
+// } while (N <= 0);
