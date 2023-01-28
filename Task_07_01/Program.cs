d814cf362c08cfbaff5dbdd6d41c6f61af59d84c
MMick <mickmail2000@gmail.com>
2023-01-28 23:42:41 +0300

    Исправили букву в Task_07_01

--- Task_07_01/Program.cs
+++ Task_07_01/Program.cs
@@ -10,4 +10,4 @@
 Console.WriteLine("Введите трёхзначное число:");
 int number = Convert.ToInt32(Console.ReadLine());
 int lastDigit = number % 10;    
-Console.WriteLine($"Последняя цтфра данного трёхзначного числа - это {number % 10}");    
+Console.WriteLine($"Последняя цифра данного трёхзначного числа - это {number % 10}");    
