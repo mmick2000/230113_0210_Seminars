7cb15bb3ef4f74e837a63d1cdf56bf5f8c806b8e
MMick <mickmail2000@gmail.com>
2023-01-29 00:04:03 +0300

    Решили Задачу 5

--- Task_05_01/Program.cs
+++ Task_05_01/Program.cs
@@ -1,2 +1,15 @@
-﻿// See https://aka.ms/new-console-template for more information
-Console.WriteLine("Hello, World!");
+﻿// 5. Напишите программу, которая 
+// 1. на вход принимает одно число (N), 
+// 2. на выходе показывает все целые числа в промежутке от -N до N.
+// 4-> "-4,-3,-2,-1, 0,1, 2,3,4"
+// 2-> "-2,-1, 0,1,2"
+
+Console.WriteLine("Введите любое целое число от нуля и больше");
+int N = Convert.ToInt32(Console.ReadLine());
+int number = 0 - N;
+while (number <= N){
+    Console.Write(number);
+    number ++;
+    if (number <= N){
+    Console.Write(", ");}
+}
