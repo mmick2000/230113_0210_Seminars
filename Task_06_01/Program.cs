83bbbce139fb33e5794e078137ce8e311a0adcff
MMick <mickmail2000@gmail.com>
2023-01-29 02:50:55 +0300

    Привёл ответ задачи 6 в соответствие с ТЗ

--- Task_06_01/Program.cs
+++ Task_06_01/Program.cs
@@ -6,7 +6,7 @@
 Console.WriteLine("Введите целое число:");
 int number = Convert.ToInt32(Console.ReadLine());
 if (number % 2 == 0){
-    Console.WriteLine("Да - введённое число чётное");
+    Console.WriteLine("Да");
 } else {
-    Console.WriteLine("Нет - введённое число нечётное");
+    Console.WriteLine("Нет");
 }
