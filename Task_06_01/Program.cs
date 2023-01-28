93c79d1b5d28a1e3decdb8f211f4976dec0f3904
MMick <mickmail2000@gmail.com>
2023-01-29 02:07:26 +0300

    Добавлена и решена Task_04_01

---
+++ Task_06_01/Program.cs
@@ -0,0 +1,10 @@
+﻿// See https://aka.ms/new-console-template for more information
+Console.WriteLine("Hello, World!");
+
+Console.WriteLine("Введите целое число:");
+int number = Convert.ToInt32(Console.ReadLine());
+if (number % 2 == 0){
+    Console.WriteLine("Да - введённое число чётное");
+} else {
+    Console.WriteLine("Нет - введённое число нечётное");
+}
