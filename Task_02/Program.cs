0ec2cd6f4510c90e880852373d528ff20922a8f1
MMick <mickmail2000@gmail.com>
2023-01-29 01:18:36 +0300

    Добавили и решили Task_02

--- Task_02/Program.cs
+++ Task_02/Program.cs
@@ -1,2 +1,15 @@
-﻿// See https://aka.ms/new-console-template for more information
-Console.WriteLine("Hello, World!");
+﻿// Task_02: Напишите программу, которая 
+// 1. на вход принимает два числа 
+// 2. выдаёт, какое число большее, а какое меньшее.
+
+// bool isFinished = false;
+
+Console.WriteLine("Введите первое целое число");
+int number1 = Convert.ToInt32(Console.ReadLine());
+Console.WriteLine("Введите воторое целое число");
+int number2 = Convert.ToInt32(Console.ReadLine());
+if (number1 > number2){
+    Console.WriteLine($"max = {number1}");
+} else {
+    Console.WriteLine($"max = {number2}");
+}
--- Task_05_01/Program.cs
+++ Task_05_01/Program.cs
@@ -3,26 +3,33 @@
 // 2. на выходе показывает все целые числа в промежутке от -N до N.
 // 4-> "-4,-3,-2,-1, 0,1, 2,3,4"
 // 2-> "-2,-1, 0,1,2"
-bool isFinished = false;
 
+bool isFinished = false;
 Console.WriteLine("Введите любое целое число больше нуля:");
-while(isFinished == false) {
-int N = Convert.ToInt32(Console.ReadLine());
-if (N > 0){
-    // do{
- int number = 0 - N;
- 
-while (number <= N){
-    Console.Write(number);
-    number ++;
-    if (number <= N){
-    Console.Write(", ");
-    isFinished = true;}
-}   
-}else {
-    Console.WriteLine("Внимательнее читай задание... и снова вводи число");
-    isFinished = false;
-}
+while (isFinished == false)
+{
+    int N = Convert.ToInt32(Console.ReadLine());
+    if (N > 0)
+    {
+        // do{
+        int number = 0 - N;
+
+        while (number <= N)
+        {
+            Console.Write(number);
+            number++;
+            if (number <= N)
+            {
+                Console.Write(", ");
+                isFinished = true;
+            }
+        }
+    }
+    else
+    {
+        Console.WriteLine("Внимательнее читай задание... и снова вводи число");
+        isFinished = false;
+    }
 }
 
 // } while (N <= 0);
