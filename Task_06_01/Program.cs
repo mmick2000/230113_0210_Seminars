6cc44b68c33a3f0cf646f4393b312ffbc6cbfc49
MMick <mickmail2000@gmail.com>
2023-01-29 02:10:19 +0300

    Добавлена, решена и отформатирована Task_06_01

--- Task_06_01/Program.cs
+++ Task_06_01/Program.cs
@@ -1,5 +1,7 @@
-﻿// See https://aka.ms/new-console-template for more information
-Console.WriteLine("Hello, World!");
+﻿// Task_06_01:
+// Напишите программу, которая 
+// 1. на вход принимает число 
+// 2. выдаёт, является ли число чётным (делится ли оно на два без остатка).
 
 Console.WriteLine("Введите целое число:");
 int number = Convert.ToInt32(Console.ReadLine());
