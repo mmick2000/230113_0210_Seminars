b593ff030ab923869367e4f375fc7277cfb049f0
MMick <mickmail2000@gmail.com>
2023-01-28 16:23:50 +0300

    Added Task_00, Task_01 b Task_02

--- Task_00/Program.cs
+++ Task_00/Program.cs
@@ -1,2 +1,16 @@
-﻿// See https://aka.ms/new-console-template for more information
-Console.WriteLine("Hello, World!");
+﻿// 0. Демонстрация решения
+// Напишите программу, которая 
+// 1. на вход принимает число 
+// 2. выдаёт его квадрат.
+// вывод результата в консоль.
+// Например:
+// 4 -> 16
+// -3 -> 9
+// -7 -> 49
+
+Console.WriteLine("Введите любое целое число, которое необходимо возвести в квадрат");
+int number = Convert.ToInt32(Console.ReadLine());
+int square  = number * number;
+
+Console.WriteLine("Квадрат числа " + number + " равно " + square);
+Console.WriteLine($"Квадрат числа {number} равно {square}");
---
+++ Task_01/Program.cs
@@ -0,0 +1,24 @@
+﻿// See https://aka.ms/new-console-template for more information
+Console.WriteLine("Hello, World!");
+
+// 01. Напишите программу, которая 
+// 1. на вход принимает два
+// числа,
+// 2.  второе чоисло возводит в квадрат,
+// 3. проверяет, является-ли первое число квадратом
+// второго.
+// а = 25, b = 5 -> да 
+// а = 2, b = 10 -> нет 
+// а = 9, b = -3 -> да 
+// а = -3 b = 9 -> нет
+
+Console.WriteLine("Введите первое целое число");
+int number1 = Convert.ToInt32(Console.ReadLine());
+Console.WriteLine("Введите воторое целое число");
+int number2 = Convert.ToInt32(Console.ReadLine());
+int square  = number2 * number2;
+if (square == number1){
+    Console.WriteLine($"Первое число {number1} равно квадрату второго числа {number2}");
+} else {
+    Console.WriteLine($"Первое число {number1} не равно квадрату второго числа {number2}");
+}
