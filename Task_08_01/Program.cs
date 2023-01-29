2b455342840dd43679a17cbac1a1895d50b30711
MMick <mickmail2000@gmail.com>
2023-01-29 03:09:40 +0300

    Добавил в Task_08_01 циклическое условие проверки/введения верного числа

--- Task_08_01/Program.cs
+++ Task_08_01/Program.cs
@@ -3,16 +3,35 @@
 // 1. на вход принимает число (N), 
 // 2. на выходе показывает все чётные числа от 1 до N.
 
-Console.WriteLine("Введите целое число:");
-int number = Convert.ToInt32(Console.ReadLine());
-int startNum = 1;
-
-while (startNum <= number)
+int isFinished = 0;
+Console.WriteLine("Введите целое число больше нуля:");
+while (isFinished == 0)
 {
-    if (startNum % 2 == 0)
+    int N = Convert.ToInt32(Console.ReadLine());
+    if (N > 0)
     {
-        Console.Write(startNum);
-        if (startNum < number) Console.Write(", ");
+        int startNum = 1;
+
+        while (startNum <= N)
+        {
+            if (startNum % 2 == 0)
+            {
+                Console.Write(startNum);
+                if (startNum < N-1)
+                {
+                    Console.Write(", ");
+                    isFinished = 1;
+
+                }
+            }
+
+            startNum++;
+
+        }
     }
-    startNum++;
+    else
+    {
+        Console.WriteLine("Внимательнее читай задание... и снова вводи число");
+        isFinished = 0;
+    }
 }
