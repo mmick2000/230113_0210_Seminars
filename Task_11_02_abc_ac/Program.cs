8b53bf0cb52b5475dd52923e50bfeb9b569503c7
Mmick <mickmail2000@gmail.com>
2023-01-31 07:03:38 +0300

    Добавили файл Task_11_02_abc_ac в котором уточн решение из Task_11_01_abc_ac

---
+++ Task_11_02_abc_ac/Program.cs
@@ -0,0 +1,32 @@
+﻿// Task_11_01_abc_ac
+// Напишите программу, которая 
+// 1. выводит случайное трёхзначное число
+// 2. удаляет вторую цифру этого числа.
+// 456 -> 4б|
+// 782->72
+// 918 -> 98
+  
+int NumRandomGeneration(int a, int b) // метод генерации числа и его выведения в консоль
+{
+    int randomNumber = new Random().Next(a, b);
+    Console.WriteLine($"Получаем случайное трёхзначное число => {randomNumber}.");
+    return randomNumber;
+}
+
+int GetNumber(int randomNumber) // метод вычисления результата
+{
+    int firstDigit = randomNumber / 100;
+    int secondDigit = randomNumber % 10;
+    int num = firstDigit * 10 + secondDigit;
+    return num;
+}
+
+void PrintNumber (int randomNumber)
+{
+int num = GetNumber(randomNumber);
+Console.WriteLine(randomNumber + " -> " + num);
+}
+
+int randomNumber = NumRandomGeneration(100, 1000);
+// Console.WriteLine($"Получаем случайное трёхзначное число => {randomNumber}.");
+PrintNumber(randomNumber);
