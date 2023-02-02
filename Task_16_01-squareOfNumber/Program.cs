5c49044f00d19a0bb47d6a518e651dd4c091436b
Mmick <mickmail2000@gmail.com>
2023-02-02 07:14:48 +0300

    Добавли и решили Task_14_01_multiplyNum7&23

---
+++ Task_16_01-squareOfNumber/Program.cs
@@ -0,0 +1,27 @@
+﻿// Task_16_01
+// Напишите программу, которая 
+// 1. принимает на вход два числа 
+// 2. проверяет, является ли одно число квадратом другого.	С
+// 5, 25 -> да
+// -4,16 -> да
+// 25, 5 -> да
+// 12 мин
+// 8,9 -> нет
+
+Console.WriteLine("Введите два числа, подтверждая ввод через Enter");
+int firstNumber = Convert.ToInt32(Console.ReadLine());
+int secondNumber = Convert.ToInt32(Console.ReadLine());
+
+bool MultipleNums(int num1, int num2)
+{
+if (num1 % num2 == 0 || num2 % num1 == 0 )
+    {
+        return true;
+    }
+    else
+    {
+        return false; }
+}
+if (MultipleNums(firstNumber, secondNumber))
+{ Console.WriteLine("Да"); }
+else { Console.WriteLine($"Нет"); }
