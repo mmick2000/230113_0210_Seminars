640d6a88298b87f1bde6183265253bc19806e0a5
Mmick <mickmail2000@gmail.com>
2023-02-03 09:50:39 +0300

    Добавил и решил Task_13_02_abcde_cOrNull

--- Task_13_01_abcde_cOrNull/Program.cs
+++ Task_13_01_abcde_cOrNull/Program.cs
@@ -5,5 +5,13 @@
 // 78 -> третьей цифры нет 
 // 32679 ->6
 
-Console.WriteLine("Введите число");
-int number = Convert.ToInt32(Console.ReadLine());
+Console.WriteLine("Введите любое число");
+int enterNumber = Convert.ToInt32(Console.ReadLine());
+int number = enterNumber;
+if (number > -100 && number < 100) {Console.WriteLine($"{number} -> третьей цифры нет");}
+else{ while (number < -999 || number > 999){
+    number = number /10;}
+}
+int thirdDigit = /* number - number /100 - */ number %10;
+// thirdDigit < 0 ? -thirdDigit : thirdDigit;
+Console.WriteLine($"{enterNumber} -> {(thirdDigit < 0 ? -thirdDigit : thirdDigit)}");
---
+++ Task_13_02_abcde_cOrNull/Program.cs
@@ -0,0 +1,30 @@
+﻿// Task_13_02_abcde_cOrNull
+// Задача 13: Напишите программу, которая 
+// 1. выводит третью цифру заданного числа или сообщает, что третьей цифры нет.	
+// 645-> 5 
+// 78 -> третьей цифры нет 
+// 32679 ->6
+
+Console.WriteLine("Введите любое число");
+
+int FindThirdDigit(int number)
+{
+    while (number < -999 || number > 999)
+    {
+        number = number / 10;
+    }
+    int thirdDigit = /* number - number /100 - */ number % 10;
+    return thirdDigit;
+}
+
+int enterNumber = Convert.ToInt32(Console.ReadLine());
+int number = enterNumber;
+
+
+if (number > -100 && number < 100) { Console.WriteLine($"{number} -> третьей цифры нет"); }
+else
+{
+    int thirdDigit = FindThirdDigit(number);
+    Console.WriteLine($"{enterNumber} -> {(thirdDigit < 0 ? -thirdDigit : thirdDigit)}");
+}
+    // thirdDigit < 0 ? -thirdDigit : thirdDigit;
