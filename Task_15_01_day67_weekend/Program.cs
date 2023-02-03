9ccda76e016432af51289c3f80abe093fe8d9bc8
Mmick <mickmail2000@gmail.com>
2023-02-03 10:48:08 +0300

    Добавили и решили Task_15_01_day67_weekend максимально через методы

--- Task_15_01_day67_weekend/Program.cs
+++ Task_15_01_day67_weekend/Program.cs
@@ -7,5 +7,29 @@
 // 7	-> да 
 // 1 -> нет
 
-Console.WriteLine("Введите число");
-int number = Convert.ToInt32(Console.ReadLine());
+bool isFinished = false;
+Console.WriteLine("Введите цифру, обозначающую день недели? для определения, выходной-ли это день");
+while (isFinished == false)
+{
+    int number = Convert.ToInt32(Console.ReadLine());
+    FindDay(number);
+}
+void FindDay(int number)
+{
+    if (number >= 1 && number <= 7)
+    {
+        bool weekDay = FindWeekend(number);
+        string day = weekDay ? "Да" : "Нет";
+        Console.WriteLine(day);
+        isFinished = true;
+    }
+    else
+    {
+        Console.WriteLine($"{number} находится за пределами диапазона дней недели - введите цифру от 1-го до 7-ми");
+        isFinished = false;
+    }
+}
+    bool FindWeekend(int number)
+    {
+        return number == 6 || number == 7;
+    }
