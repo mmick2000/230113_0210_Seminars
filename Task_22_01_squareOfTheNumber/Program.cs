b46acdbd0c295fe2d163ffcd9784c4767aef2294
Mmick <mickmail2000@gmail.com>
2023-02-15 05:23:36 +0300

    Добавили и решили Task_22_01_squareOfTheNumber, а ранее 18 и 20

---
+++ Task_22_01_squareOfTheNumber/Program.cs
@@ -0,0 +1,28 @@
+﻿// Task_22_01_squareOfTheNumber
+// Задача 22: Напишите программу, которая 
+// 1. принимает на вход число (N) 
+// 2. выдаёт таблицу квадратов чисел от 1 до N.
+// 5 -> 1, 4, 3, 16, 25.
+// 2 -> 1,4
+
+Console.WriteLine ("Введите число больше нуля");
+// int number = Convert.ToInt32(Console.ReadLine());
+int.TryParse(Console.ReadLine(), out int number);
+if(number < 1) {
+    Console.WriteLine ("Введите число в соответствии с условием");
+    return;
+}
+// int n = 1;
+/* else */ QuadOfNumber(number);
+void QuadOfNumber(int n)
+{
+Console.Write(number + " -> ");
+    int count =  1;
+    while(count <= n)
+    {
+        Console.Write($"{count*count/* ,3 */}");
+       if (count < n) Console.Write(", ");
+        count++;
+    }
+}
+
