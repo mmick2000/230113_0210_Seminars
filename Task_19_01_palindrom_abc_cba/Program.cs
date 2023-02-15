464e7af6c532e268772406145556c449f5804a8c
Mmick <mickmail2000@gmail.com>
2023-02-15 07:23:40 +0300

    Solved Task_19

--- Task_19_01_palindrom_abc_cba/Program.cs
+++ Task_19_01_palindrom_abc_cba/Program.cs
@@ -6,3 +6,31 @@
 // 23432 -> да 
 // 12821-> да
 
+
+
+Console.WriteLine("Введите пятизначное число:");
+int.TryParse(Console.ReadLine(), out int number);  
+if (number < 10000 || number > 99999) {
+    Console.WriteLine("Некорректный ввод");
+    return;
+}
+int reverseNum = ReverseNum(number);
+Ask (reverseNum, number);
+
+string Ask(int a, int b){
+if (reverseNum == number)
+return "Да";
+else return "Нет";
+}
+Console.WriteLine(number + " -> " + Ask (reverseNum, number));
+
+int ReverseNum(int num)
+{
+    int result = 0;
+    while (num > 0)
+    {
+        result = result * 10 + num % 10;
+        num /= 10;
+    }
+        return result;
+}
